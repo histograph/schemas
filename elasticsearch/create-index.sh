8bbeefc5e23b87a74c4fefd1ff6c3e60ab13809b
rutgervw <rutgervanwilligen@gmail.com>
2015-03-20 11:37:22 +0100

    Include mappings key in json

--- elasticsearch/create-index.sh
+++ elasticsearch/create-index.sh
@@ -1,3 +1,2 @@
 # TODO: read host and index name from config!
-curl -XPUT 'http://localhost:9200/histograph/'
-curl -XPUT 'http://localhost:9200/histograph/_mapping/pit'  --data @pit.json
+curl -XPUT 'http://localhost:9200/histograph/'  --data @pit.json
