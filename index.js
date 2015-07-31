6666e7244c38771756f50c55bf9c29f5086958d0
Bert Spaan <bert.spaan@gmail.com>
2015-07-31 17:20:44 +0200

    new package.json, and schemas module now loads graphmalizer config

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+var path = require('path');
+
 module.exports = {
   dataset: require(path.join(__dirname, 'json', 'dataset.schema.json')),
   pits: require(path.join(__dirname, 'json', 'pits.schema.json')),
   relations: require(path.join(__dirname, 'json', 'relations.schema.json')),
+  graphmalizer: require(path.join(__dirname, 'graphmalizer', 'graphmalizer.config.json'))
 };
